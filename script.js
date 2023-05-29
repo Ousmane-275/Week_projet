ceaeb33d4ee613557a19a5826dd08e0c6c41db0f
Ousmane.275 <codeur.275@gmail.com>
2023-05-29 14:41:20 +0200

    modal page

---
+++ script.js
@@ -0,0 +1,25 @@
+// Obtenir le modal
+var modal = document.getElementById("myModal");
+
+// Obtenir l'élément déclencheur pour ouvrir le modal
+var btn = document.getElementById("myBtn");
+
+// Obtenir l'élément <span> qui ferme le modal
+var span = document.getElementsByClassName("close")[0];
+
+// Événement de clic pour ouvrir le modal
+btn.onclick = function() {
+  modal.style.display = "block";
+};
+
+// Événement de clic pour fermer le modal
+span.onclick = function() {
+  modal.style.display = "none";
+};
+
+// Événement de clic en dehors du modal pour le fermer
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display ="none";
+}
+};
